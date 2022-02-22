8cde1387b5f46322ea6b927df6af9db8b1edde92
Manish-162403 <naamcool420@gmail.com>
2022-02-22 10:53:18 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
-let obj = require('../loggerFolder/logger')
+let obj = require('../loggerFolder/logger');
 let obj1 = require('../util/helper');
-const myObj = require('../validator/formatter')
+const myObj = require('../validator/formatter');
+const lodash = require('lodash')
 const express = require('express');
 
 const router = express.Router();
@@ -12,21 +13,37 @@ router.get('/test-me', function (req, res) {
     obj.welcome()
     console.log(obj.welcome)
 
-    myObj.myTrim()
-
-
-    obj1.batchInfo()
-
-   obj1.dateInfo()
-   //console.log(dateInfo)
-
-   obj1.month()
-    //console.log(month)
-
+  console.log('printDate: ', helper.printDate);
+  console.log('printMonth: ', helper.printMonth+1);
+  helper.printBatchInfo("thorium, w3d1, the topic for today is Nodejs module system.")
     
-    
+  myObj.myTrim()
 
     res.send('My first ever api!')
+});
+
+router.get('/hello', function (req, res){
+   let arr = ["jan","feb", "mar", "apr", "may","jun","jul","aug","sept","oct", "nov", "dec"]
+   const output = lodash.chunk(arr, [size = 3])
+   console.log(output);
+
+   let arr1 = [1,3,5,7,9,11,13,15,17,19]
+   const out1= lodash.tail(arr1, [n=9])
+   console.log(out1)
+
+   let a1 = [1,2,3,4]
+   let b1 = [1,2,65,4]
+   let c1 = [1,23,45,3]
+   let d1 = [12,23,12,4]
+   let e1 = [1,23,4,3]
+
+   const out3 = lodash.union(a1,b1,c1,d1,e1)
+   console.log(out3)
+   
+   
+   const arr3 = [["horror","The Shining"],["drama","Titanic"],["thriller","ShutterIsland"],["fantasy","Pans Labyrinth"]]
+   const out4 = lodash.fromPairs(arr3)
+   console.log(out4);
 });
 
 module.exports = router;
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,23 +1,10 @@
-function dateInfo(){
-    let a = newDate()
-    const newDate = a.getDate();
-    console.log(dateInfo)
+let theDay  = new Date().toLocaleDateString()
+const d = new Date();
+let month = d.getMonth();
+function getBatchInfo(message){
+    console.log(message);
 }
 
-
-function month(){
-    const currentMonth = a.getMonth()
-    console.log("" + currentMonth) 
-}
-
-function batchInfo(){
-    let batchName = "Thorium"
-    let week = "w3"
-    let day = "D1"
-    let infoBatch = " " + batchName + " #" + week + day + "the topic for today is Nodejs module system"
-    console.log(infoBatch)
-}
-
-module.exports.dateInfo=dateInfo;
+module.exports.dateInfo= theDay;
 module.exports.month = month;
-module.exports.batchInfo= batchInfo;
+module.exports.batchInfo= getBatchInfo;
