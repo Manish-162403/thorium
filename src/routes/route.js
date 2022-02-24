46f48a8538018c7b8fee5de2d66d55a45f32cf5f
Manish-162403 <naamcool420@gmail.com>
2022-02-24 23:54:47 +0530

    voting

--- src/routes/route.js
+++ src/routes/route.js
@@ -50,36 +50,59 @@ router.post("/post-query-2", function (req, res) {
 // also return an array consisting of only the person that can vote
 
 //  take this as sample for array of persons:
-// let persons= [
-//     {
-//     name: "PK",
-//     age: 10,
-//     votingStatus: false
-// },
-// {
-//     name: "SK",
-//     age: 20,
-//     votingStatus: false
-// },
-// {
-//     name: "AA",
-//     age: 70,
-//     votingStatus: false
-// },
-// {
-//     name: "SC",
-//     age: 5,
-//     votingStatus: false
-// },
-// {
-//     name: "HO",
-//     age: 40,
-//     votingStatus: false
-// }
-// ]
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+},
+{
+    name: "SK",
+    age: 20,
+    votingStatus: false
+},
+{
+    name: "AA",
+    age: 70,
+    votingStatus: false
+},
+{
+    name: "SC",
+    age: 5,
+    votingStatus: false
+},
+{
+    name: "HO",
+    age: 40,
+    votingStatus: false
+}
+]
+
+    router.post("/voting", function (req, res) {
+        let votingAge = req.query.votingAge
+    
+        let a=[];
+        for (let i = 0; i < persons.length; i++) {
+    
+            if (persons[i].age > votingAge) {
+            
+                persons[i].votingStatus = true
+                a.push(persons[i])
+            }
+        }
+    if (a.length>0)
+    {
+        return res.send(a)
+    }
+    else{
+        return res.send("no member found")
+    }
+    
+  
 
 
 
+});
 
 
 module.exports = router;
