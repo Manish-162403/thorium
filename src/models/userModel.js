556033bf448b394087688541aa80d3f55bc47dfd
Manish-162403 <naamcool420@gmail.com>
2022-03-01 01:44:07 +0530

    assignment 28 feb

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,19 +8,7 @@ const userSchema = new mongoose.Schema( {
         unique: true,
         required: true
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema) //users
@@ -29,3 +17,18 @@ module.exports = mongoose.model('User', userSchema) //users
 
 // String, Number
 // Boolean, Object/json, array
+
+
+// emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
