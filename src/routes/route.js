c4ca4678da24b3c0ccf719d4927510358d814e9d
Manish-162403 <naamcool420@gmail.com>
2022-03-09 01:00:09 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const jsonwebtoken = require('jsonwebtoken');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController= require("../controllers/userController");
+const tokenCheck = require("../Middleware/auth");
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+
 
 router.post("/users", userController.createUser  )
 
@@ -13,6 +16,9 @@ router.post("/login", userController.loginUser)
 //The userId is sent by front end
 router.get("/users/:userId", userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",tokenCheck.tokenCheck, userController.updateUser)
 
-module.exports = router;
+router.delete("/users/:userId",tokenCheck.tokenCheck, userController.deleteUser)
+
+
+module.exports = router;  
